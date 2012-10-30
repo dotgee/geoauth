ea7d910d0d9e9ae4dca4582d32036cdb12bd181c
Philippe HUET <philippe@dotgee.fr>
2012-10-30 10:06:12 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoauth::Application.config.secret_token = '0e3767cea8df5ff401657d8dbeaa5025a3cfd4b715cdad1cc1b64f1ee4ee3284bab86540a6957015cb0b6a72c89e1d8b2c83db672f3d78a2d304e0585840a913'
