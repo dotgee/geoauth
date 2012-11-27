42846a39c1caaad2e9e8280dfae8db9739c3af07
Philippe HUET <philippe@dotgee.fr>
2012-11-27 14:04:20 +0100

    Ajout roles et admin users.

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,13 @@
+module Admin
+  class BaseController < ApplicationController
+    before_filter :authenticate_user!, :check_admin_role!
+
+    private
+      def check_admin_role!
+        if current_user.nil? || !current_user.has_role?(:ROLE_ADMINISTRATOR)
+          redirect_to :root
+        end
+        true
+      end
+  end
+end
