0a382439c0591efb8deea4b2569f732c71f584da
Philippe HUET <philippe@dotgee.fr>
2013-02-19 16:47:40 +0100

    Add role properties

---
+++ app/models/role_properties.rb
@@ -0,0 +1,7 @@
+class RoleProperties < ActiveRecord::Base
+  self.table_name :role_props
+
+  belongs_to :role
+
+  attr_accessible :rolename, :propname, :propvalue
+end
--- app/models/user_property.rb
+++ app/models/user_property.rb
@@ -1,3 +1,4 @@
 class UserProperty < ActiveRecord::Base
+  self.table_name :user_props
   attr_accessible :propname, :propvalue, :username, :users
 end
